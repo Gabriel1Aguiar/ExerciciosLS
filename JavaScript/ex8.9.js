a9d52fa1a34698dbadd842539a6d99aeb169237f
Gabriel1Aguiar <gabriel.aguiardn23@gmail.com>
2020-09-14 12:01:45 -0300

    Exercicio function

---
+++ JavaScript/ex8.9.js
@@ -0,0 +1,43 @@
+/*
+//Questão 1
+function invert (x){
+    return x.reverse()
+}
+
+let x = [3,2,2,4,3]
+console.log(x.join(''))
+invert(x)
+console.log(x.join(''))
+
+//Questão 2
+function primo(x){
+    if (x%2===0 || x === 1){
+        return (x+' não é primo')
+    }else{
+        return (x+' é primo')
+    }
+}
+
+let x = 1
+console.log(primo(x))
+
+//Questão 3 
+
+//Questão 4
+function acimaMedia (x){
+    return x >= 7
+}
+
+function abaixoMedia (x){
+    return x < 7
+}
+
+let notas = [7,8,9,5,4,10,3]
+let acima = notas.filter(acimaMedia)
+let menor = notas.filter(abaixoMedia)
+
+console.log(acima + ' aprovados')
+console.log(menor + ' reprovados')
+*/
+//Questão 5
+
